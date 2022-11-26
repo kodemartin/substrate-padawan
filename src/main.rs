b3c6550d1d7925d72993c1e38f8a8f96853900bd
Konstantinos Demartinos <kodemartin@protonmail.ch>
2022-11-26 18:19:25 +0200

    Implement simple behaviour to establish connection

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use std::net::Ipv4Addr;
 
 use clap::Parser;
 use futures::StreamExt;
-use libp2p::swarm::{Swarm, SwarmEvent};
+use libp2p::swarm::{dummy, Swarm, SwarmEvent};
 use libp2p::{identity, multiaddr, PeerId};
-use substrate_padawan::PingAliveBehaviour;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
@@ -39,19 +38,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let keypair = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(keypair.public());
     tracing::info!("Local peer id: {:?}", peer_id);
+
     let transport = libp2p::tokio_development_transport(keypair)?;
-    let behaviour = PingAliveBehaviour::default();
+    let behaviour = dummy::Behaviour;
 
     let mut swarm = Swarm::with_tokio_executor(transport, behaviour, peer_id);
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-    let address = remote.to_string();
-    swarm.dial(remote)?;
-    tracing::info!("Dialed remote: {}", address);
+    swarm.dial(remote.clone())?;
+    info!("Dialed remote: {}", remote);
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => info!("Listening on {:?}", address),
-            SwarmEvent::Behaviour(event) => info!("{:?}", event),
+            SwarmEvent::ConnectionEstablished {
+                peer_id, endpoint, ..
+            } => {
+                info!("Established connection with {:?}", peer_id);
+                if endpoint.get_remote_address() == &remote {
+                    break;
+                }
+            }
+            SwarmEvent::ConnectionClosed {
+                peer_id, endpoint, ..
+            } => {
+                info!("Connection with {:?} is closed", peer_id);
+                if endpoint.get_remote_address() == &remote {
+                    break;
+                }
+            }
             _ => {}
         }
     }
+    Ok(())
 }
